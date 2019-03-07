6c6cdf7f9cdab2d32696b192c0100008d1910de1
SteveWillowby <jhibshma@nd.edu>
2019-03-07 14:36:39 -0500

    Reworked for efficiency and added framework to run per-node tests.

--- test.py
+++ test.py
@@ -62,52 +62,124 @@ def make_graph_with_same_degree_dist(G):
             done = True
     return G_prime
 
-# Returns all the data from running the LP
-def run_LP_with_maxs(G, G_prime, G_maxs=None, G_prime_maxs=None, goal_node=None):
-    N = len(G.nodes())
-
-    # Assume weights are indexed by G and then by G_prime, so w_10 is weight from G[1] to G_prime[0]
-    c = [1 for n in range(0, N*N)]
-    if goal_node is not None: # If there's one node (in G, not G_prime) we really want to minimize
-        for i in range(0, N):
-            c[N*goal_node + i] += N*N
-
-    SP = dict(all_pairs_shortest_path_length(G))
-    SP_prime = dict(all_pairs_shortest_path_length(G_prime))
+def lp_without_maxs_or_goal(N, SPG, SPG_prime):
     A = []
     b = []
     for i in range(0, N):
         for j in range(i, N):
             for k in range(0, N):
                 for l in range(k, N):
-                    new_constraint = [0 for n in range(0, N*N)] # w_ik and w_jl
-                    new_constraint[N*i + k] = -1
-                    new_constraint[N*j + l] = -1
+                    new_constraint = [0 for n in range(0, N * N)]  # w_ik and w_jl
+                    new_constraint[N * i + k] = -1
+                    new_constraint[N * j + l] = -1
                     A.append(new_constraint)
-                    b.append(-abs(SP[i][j] - SP_prime[k][l]))
-                    new_constraint = [0 for n in range(0, N*N)] # w_il and w_jk
+                    b.append(-abs(SPG[i][j] - SPG_prime[k][l]))
+                    new_constraint = [0 for n in range(0, N * N)]  # w_il and w_jk
                     new_constraint[N * i + l] = -1
                     new_constraint[N * j + k] = -1
                     A.append(new_constraint)
-                    b.append(-abs(SP[i][j] - SP_prime[k][l]))
+                    b.append(-abs(SPG[i][j] - SPG_prime[k][l]))
+    return (A, b)
 
-    if G_maxs is not None:
-        if(len(G_maxs) != N):
-            print("Error! len(G_maxs) != N")
+def maxs_matrix(N):
+    A = []
+    for i in range(0, N):
+        new_constraint = [1 if N*i <= j < N * (i+1) else 0 for j in range(0, N*N)]
+        A.append(new_constraint)
+    for i in range(0, N):
+        new_constraint = [1 if j % N == i else 0 for j in range(0, N * N)]
+        A.append(new_constraint)
+    return A
+
+def goal_vector(N, target_min=None):
+    c = [1 for n in range(0, N * N)]
+    if target_min is not None:  # If there's one node (in G, not G_prime) we really want to minimize
         for i in range(0, N):
-            new_constraint = [1 if N*i <= j and j < N * (i+1) else 0 for j in range(0, N*N)]
-            #print(new_constraint)
-            A.append(new_constraint)
-            b.append(G_maxs[i])
-    if G_prime_maxs is not None:
-        if (len(G_prime_maxs) != N):
-            print("Error! len(G_prime_maxs) != N")
+            c[N * target_min + i] += N
+    return c
+
+def goal_vector_prime(N, target_min=None):
+    c = [1 for n in range(0, N * N)]
+    if target_min is not None:  # If there's one node (in G, not G_prime) we really want to minimize
         for i in range(0, N):
-            new_constraint = [1 if j % N == i else 0 for j in range(0, N * N)]
-            #print(new_constraint)
-            A.append(new_constraint)
-            b.append(G_prime_maxs[i])
-    return linprog(c, A_ub=A, b_ub=b, method="interior-point", options={"disp":False, "maxiter":N*N*N*N*100})
+            c[N * i + target_min] += N
+    return c
+
+def weight_caps(N, result):
+    caps = [0.01 for n in range(0, N)]  # TODO: The 0.01 is for slack. It should probably be a function of N.
+    for i in range(0, N * N):
+        caps[int(i / N)] += result.x[i]
+    return caps
+
+def weight_caps_prime(N, result):
+    caps = [0.01 for n in range(0, N)]  # TODO: The 0.01 is for slack. It should probably be a function of N.
+    for i in range(0, N * N):
+        caps[i % N] += result.x[i]
+    return caps
+
+def iso_check(G, G_prime):
+    N = len(G.nodes())
+    SPG = dict(all_pairs_shortest_path_length(G))
+    SPG_prime = dict(all_pairs_shortest_path_length(G_prime))
+
+    # First run the graphs against themselves
+    (AG, bG) = lp_without_maxs_or_goal(N, SPG, SPG)
+    c = goal_vector(N)
+    G_with_G = linprog(c, A_ub=AG, b_ub=bG, method="interior-point", options={"disp":False, "maxiter":N*N*N*N*100})
+
+    (AG_prime, bG_prime) = lp_without_maxs_or_goal(N, SPG_prime, SPG_prime)
+    G_prime_with_G_prime = linprog(c, A_ub=AG_prime, b_ub=bG_prime, method="interior-point", options={"disp":False, "maxiter":N*N*N*N*100})
+
+    # First check: Are sums not equal?
+    if abs(G_with_G.fun - G_prime_with_G_prime.fun) > 0.01:  # TODO: Replace this constant.
+        return False
+    print(G_with_G.fun)
+    print(G_prime_with_G_prime.fun)
+
+    print ("Going on to second round test.")
+    # Then run them against each other, holding G's caps from before:
+    (AGGP, bGGP) = lp_without_maxs_or_goal(N, SPG, SPG_prime)
+    A_weight_caps = maxs_matrix(N)
+    A = AGGP + A_weight_caps
+    b_G1_caps = weight_caps(N, G_with_G)
+    b_G2_caps = [N*N for n in range(0, N)]
+    b = bGGP + b_G1_caps + b_G2_caps
+    G_with_G_prime = linprog(c, A_ub=A, b_ub=b, method="interior-point", options={"disp":False, "maxiter":N*N*N*N*100})
+
+    # Second check: Are sums not equal or did it fail??
+    if G_with_G_prime.status != 0 or abs(G_with_G.fun - G_with_G_prime.fun) > 0.01:  # TODO: Replace this constant.
+        print("Second check failed.")
+        print(G_with_G_prime)
+        return False
+
+    # Use G_with_G_prime to get caps for G_prime, then see if it can use those with itself.
+    A = AG_prime + A_weight_caps
+    b_G1_caps = weight_caps_prime(N, G_with_G_prime)
+    b = bG_prime + b_G1_caps + b_G2_caps
+    G_prime_with_G_prime = linprog(c, A_ub=A, b_ub=b, method="interior-point", options={"disp":False, "maxiter":N*N*N*N*100})
+
+    # Third check: Can G_prime match with itself using the limits from G_with_G_prime?
+    if G_prime_with_G_prime.status != 0 or abs(G_with_G.fun - G_prime_with_G_prime.fun) > 0.01:  # TODO: Replace this constant.
+        print("Third check failed.")
+        return False
+
+    # Lastly, do the full operation.
+    #for i in range(0, N):
+
+
+    return True
+
+def old_counter_example():
+    G = nx.Graph()
+    G_prime = nx.Graph()
+    for i in range(0, 8):
+        G.add_node(i)
+        G_prime.add_node(i)
+    for G_edge in [(0, 2), (0, 6), (1, 3), (1, 7), (2, 4), (2, 6), (2, 7), (3, 5), (3, 6), (3, 7), (4, 7), (5, 7), (6, 7)]:
+        G.add_edge(G_edge[0], G_edge[1])
+    for G_prime_edge in [(0, 7), (0, 5), (1, 4), (1, 7), (2, 6), (2, 5), (3, 4), (3, 7), (4, 7), (4, 6), (5, 6), (5, 7), (6, 7)]:
+        G_prime.add_edge(G_prime_edge[0], G_prime_edge[1])
+    return (G, G_prime)
 
 def permute_labels_only(G):
     N = len(G.nodes())
@@ -128,7 +200,7 @@ for i in range(1,15):
         #sequence = [2, 2, 2, 2, 6, 4, 4, 4, 4]  # Set sequence
         #G=nx.configuration_model(sequence)
 
-        G=nx.erdos_renyi_graph(10,0.4)
+        G=nx.erdos_renyi_graph(8,0.4)
         #G=nx.watts_strogatz_graph(10,3,0.3)
         #G=nx.barabasi_albert_graph(10,2)
 
@@ -141,17 +213,11 @@ for i in range(1,15):
         G_prime = make_graph_with_same_degree_dist(G)
         # G_prime = permute_labels_only(G)
 
-    # Make prediction
-    G_with_G = run_LP_with_maxs(G, G)
-    G_prime_with_G_prime = run_LP_with_maxs(G_prime, G_prime)
-    N = len(G.nodes())
-    G_maxs = [0.01 for n in range(0, N)]  # IMPORTANT: The 0.01 is for slack. It should probably be a function of N.
-    G_prime_maxs = [0.01 for n in range(0, N)]  # IMPORTANT: The 0.01 is for slack. It should probably be a function of N.
-    for i in range(0, N*N):
-        G_maxs[int(i / N)] += G_with_G.x[i]
-        G_prime_maxs[int(i / N)] += G_prime_with_G_prime.x[i]
-    G_with_G_prime = run_LP_with_maxs(G, G_prime, G_maxs=G_maxs, G_prime_maxs=G_prime_maxs)
-    predict_iso = G_with_G_prime.status == 0 and abs(G_with_G_prime.fun - G_with_G.fun) < 0.00001
+    (G, G_prime) = old_counter_example()
+    #print(G.edges())
+    #print(G_prime.edges())
+    #G_prime = permute_labels_only(G_prime)
+    predict_iso = iso_check(G, G_prime)
 
     # Get actual result
     GM = isomorphism.GraphMatcher(G, G_prime)
@@ -160,7 +226,7 @@ for i in range(1,15):
     if predict_iso == actual_iso:
         print("\nCorrect!")
         print(actual_iso)
-        print("G_with_G.fun")
+        """print("G_with_G.fun")
         print(G_with_G.fun)
         print("G_with_G.status")
         print(G_with_G.status)
@@ -172,15 +238,23 @@ for i in range(1,15):
         print(G_with_G_prime.status)
         print("G_with_G_prime.fun")
         print(G_with_G_prime.fun)
-        print("")
+        print("")"""
     else:
         print("Incorrect!")
         print("Actual:")
         print(actual_iso)
-        print("\nG_with_G:")
+        """print("\nG_with_G:")
         print(G_with_G)
         print("\nG_prime_with_G_prime:")
         print(G_prime_with_G_prime)
         print("\nG_with_G_prime:")
-        print(G_with_G_prime)
-
+        print(G_with_G_prime)"""
+        print("G's edges:")
+        print(G.edges())
+        print("G_prime's edges:")
+        print(G_prime.edges())
+        # break
+        # G's edges:
+        # [(0, 1), (0, 3), (1, 2), (1, 3), (1, 7), (2, 6), (3, 4), (3, 7), (4, 6), (5, 7)]
+        # G_prime's edges:
+        # [(0, 7), (1, 5), (1, 7), (2, 5), (2, 6), (3, 6), (3, 7), (4, 7), (4, 5), (5, 6)]
