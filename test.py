7fd987eb01738ee89ab7f6f16f82810c24a7d5be
SteveWillowby <jhibshma@nd.edu>
2019-03-07 14:50:53 -0500

    Another failing example in comments at end.

--- test.py
+++ test.py
@@ -213,7 +213,7 @@ for i in range(1,15):
         G_prime = make_graph_with_same_degree_dist(G)
         # G_prime = permute_labels_only(G)
 
-    (G, G_prime) = old_counter_example()
+    #(G, G_prime) = old_counter_example()
     #print(G.edges())
     #print(G_prime.edges())
     #G_prime = permute_labels_only(G_prime)
@@ -258,3 +258,9 @@ for i in range(1,15):
         # [(0, 1), (0, 3), (1, 2), (1, 3), (1, 7), (2, 6), (3, 4), (3, 7), (4, 6), (5, 7)]
         # G_prime's edges:
         # [(0, 7), (1, 5), (1, 7), (2, 5), (2, 6), (3, 6), (3, 7), (4, 7), (4, 5), (5, 6)]
+
+        # Or:
+        # G's edges:
+        #[(0, 1), (0, 2), (0, 4), (0, 7), (1, 3), (1, 4), (1, 7), (2, 6), (2, 7), (3, 4), (3, 6), (3, 7), (4, 5), (4, 6), (5, 7), (6, 7)]
+        #G_prime's edges:
+        #[(0, 5), (0, 7), (1, 7), (1, 4), (1, 3), (2, 4), (2, 5), (2, 6), (2, 7), (3, 6), (3, 5), (3, 4), (4, 7), (5, 7), (5, 6), (6, 7)]
