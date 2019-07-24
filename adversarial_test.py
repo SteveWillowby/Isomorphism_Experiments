9838052b62fe66b023427fe6e832a622cf69da18
Steve Willowby <justus.i.hibshman@gmail.com>
2019-07-24 14:24:25 -0400

    This shows v2 to be exponentially slow in some cases.

---
+++ adversarial_test.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import time
+from Gods_Way_Is_The_Best_Way import *
+
+for size_limit in range(10, 21):
+    next_size = 4.0
+    G = nx.Graph()
+    for i in range(0, size_limit):
+        G.add_node(i)
+        if i + 1 >= next_size:
+            for j in range(0, i):
+                G.add_edge(j, i)
+            next_size += 2.5
+            # print(len(G.edges()) - (i * (i - 1)) / 4)
+
+    start = time.time()
+    GG = GGraph(G)
+    end = time.time()
+    print("Size %s: Time: %s" % (size_limit, end - start))
