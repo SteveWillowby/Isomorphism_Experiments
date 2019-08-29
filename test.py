b9cd01672345dc25ff07c266afd685462d4232fe
Steve Willowby <justus.i.hibshman@gmail.com>
2019-08-29 11:10:56 -0400

    Moved a bunch of files. Need to update include statements.

--- test.py
+++ test.py
@@ -2,16 +2,8 @@ import networkx as nx
 from networkx import utils
 from networkx.algorithms.bipartite.generators import configuration_model
 from networkx.algorithms import isomorphism
-from networkx.algorithms.shortest_paths.unweighted import all_pairs_shortest_path_length
 from networkx.algorithms.components import is_connected
 import numpy as np
-from automorph_method import *
-from automorph_method_v2 import *
-from automorph_method_v3 import *
-from some_srgs import *
-from Gods_way_is_the_best_way import *
-from faster_Gway import *
-from neighbors_revisited import *
 from faster_neighbors_revisited import *
 
 def make_graph_with_same_degree_dist(G):
@@ -80,10 +72,10 @@ A4 = graph_from_srg_string(GRAPH_STRING_A4)
 
 COMPARISONS = [(A1,A2),(A1,A3),(A1,A4),(A2,A3),(A2,A4),(A3,A4)]
 
-base_0100_a = nx.read_adjlist("sat_cfi_dim/sat_cfi_base_0100_a.edge_list", create_using=nx.Graph, nodetype=int)
-base_0100_b = nx.read_adjlist("sat_cfi_dim/sat_cfi_base_0100_b.edge_list", create_using=nx.Graph, nodetype=int)
-base_1000_a = nx.read_adjlist("sat_cfi_dim/sat_cfi_base_1000_a.edge_list", create_using=nx.Graph, nodetype=int)
-base_1000_b = nx.read_adjlist("sat_cfi_dim/sat_cfi_base_1000_b.edge_list", create_using=nx.Graph, nodetype=int)
+base_0100_a = nx.read_adjlist("various_tests/sat_cfi_dim/sat_cfi_base_0100_a.edge_list", create_using=nx.Graph, nodetype=int)
+base_0100_b = nx.read_adjlist("various_tests/sat_cfi_dim/sat_cfi_base_0100_b.edge_list", create_using=nx.Graph, nodetype=int)
+base_1000_a = nx.read_adjlist("various_tests/sat_cfi_dim/sat_cfi_base_1000_a.edge_list", create_using=nx.Graph, nodetype=int)
+base_1000_b = nx.read_adjlist("various_tests/sat_cfi_dim/sat_cfi_base_1000_b.edge_list", create_using=nx.Graph, nodetype=int)
 base_0100_a = nx.Graph(base_0100_a)
 base_0100_b = nx.Graph(base_0100_b)
 base_1000_a = nx.Graph(base_1000_a)
@@ -116,7 +108,7 @@ for i in range(0, len(COMPARISONS)):
     """
 
     (G, G_prime) = COMPARISONS[i]
-    G_prime = permute_labels_only(G)
+    #G_prime = permute_labels_only(G)
     print("Starting prediction")
     c_desc_G = FasterNeighborsRevisited(G)
     print("...")
@@ -125,46 +117,17 @@ for i in range(0, len(COMPARISONS)):
     predict_iso = c_desc_G == c_desc_G_prime
     print("Got prediction: %s" % predict_iso)
     # print(c_desc_G.mapping_to_labels)
-    exit(0)
 
     # Get actual result
-    # GM = isomorphism.GraphMatcher(G, G_prime)
-    # actual_iso = GM.is_isomorphic()
-    actual_iso = False
+    GM = isomorphism.GraphMatcher(G, G_prime)
+    actual_iso = GM.is_isomorphic()
+    # actual_iso = False
 
     if predict_iso == actual_iso:
         print("\nCorrect!")
         print(actual_iso)
-        """print("G_with_G.fun")
-        print(G_with_G.fun)
-        print("G_with_G.status")
-        print(G_with_G.status)
-        print("G_prime_with_G_prime.fun")
-        print(G_prime_with_G_prime.fun)
-        print("G_prime_with_G_prime.status")
-        print(G_prime_with_G_prime.status)
-        print("G_with_G_prime.status")
-        print(G_with_G_prime.status)
-        print("G_with_G_prime.fun")
-        print(G_with_G_prime.fun)
-        print("")"""
     else:
         print("Incorrect!")
         print("Actual:")
         print(actual_iso)
-        """print("\nG_with_G:")
-        print(G_with_G)
-        print("\nG_prime_with_G_prime:")
-        print(G_prime_with_G_prime)
-        print("\nG_with_G_prime:")
-        print(G_with_G_prime)
-        print("G's edges:")
-        print(G.edges())
-        print("G_prime's edges:")
-        print(G_prime.edges())"""
-        # break
-        #G's edges:
-        #[(0, 2), (1, 3), (1, 4), (1, 7), (2, 3), (2, 5), (2, 6), (3, 5), (3, 7), (4, 6), (5, 6)]
-        #G_prime's edges:
-        #[(0, 3), (1, 5), (1, 7), (2, 4), (2, 6), (3, 6), (3, 7), (4, 5), (4, 6), (5, 7), (6, 7)]
 
