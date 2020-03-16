6e7ff0d178f951bb0d8f63b83d7724f3fd4ca446
Steve Willowby <justus.i.hibshman@gmail.com>
2020-03-16 19:02:30 -0400

    Made the graph gen part fancier so that it would always* avoid duplicate graphs.

--- hunt_for_k3_graph.py
+++ hunt_for_k3_graph.py
@@ -4,8 +4,11 @@ import graph_utils
 import alg_utils
 from weisfeiler_lehman import WL
 from corneil_thesis import QuotientGraph
+import sys
 
-def enumerate_n_choose_k_graph_pairings(n, k):
+N = int(sys.argv[1])
+
+def enumerate_n_choose_k_graph_pairings(n, k, N):
     output_bank = []
 
     basic_graph = nx.Graph()
@@ -25,11 +28,59 @@ def enumerate_n_choose_k_graph_pairings(n, k):
     unpaired_B_nodes = [i + len(basic_graph.nodes()) for i in unpaired_A_nodes]
     bank_of_already_seen_graph_types = set()
     finished_graphs = []
-    N = 0
-    print("Running with N=%d" % N)
-    enumerate_n_choose_k_graph_pairings_helper(N, disconnected_graph, unpaired_A_nodes, unpaired_B_nodes,\
+    # print("Running with N=%d" % N)
+    fancy_enumerate_n_choose_k_graph_pairings_helper(disconnected_graph, unpaired_A_nodes, unpaired_B_nodes,\
         bank_of_already_seen_graph_types, finished_graphs)
     return finished_graphs
+
+def fancy_enumerate_n_choose_k_graph_pairings_helper(current_graph, unpaired_A_nodes, unpaired_B_nodes, bank_of_already_seen_graph_types, finished_graphs):
+    canon_A = True
+    canon_B = False
+    best_canon = None
+    trusted = 0
+    highest_k = -1
+    while trusted < 5:  # Go until it uses some k or lower k's x times in a row.
+        for k in range(0, len(current_graph.nodes()) - 1):
+            canon_A = KTupleTest(current_graph, k, mode="Master")
+            canon_B = KTupleTest(graph_utils.permute_node_labels(current_graph), k, mode="Master")
+            if canon_A.matrix == canon_B.matrix:
+                if k > highest_k:
+                    trusted = 0
+                    highest_k = k
+                    best_canon = canon_A
+                else:
+                    trusted += 1
+                break
+    if highest_k >= 2:
+        print("highest_k = %d" % highest_k)
+
+    # Trust that we now have a TRUE canon and not just a lucky canon.
+    canon_matrix = tuple([tuple(row) for row in best_canon.matrix])
+    if canon_matrix in bank_of_already_seen_graph_types:
+        return
+    bank_of_already_seen_graph_types.add(canon_matrix)
+
+    if len(unpaired_A_nodes) == 0:
+        finished_graphs.append(current_graph)
+        if len(finished_graphs) % 200 == 0:
+            print(len(finished_graphs))
+        return
+
+    canon_colors = best_canon.internal_labels
+    i_colors = set()
+    for i in range(0, len(unpaired_A_nodes)):
+        # Only use one of each orbit from the first half of the graph, but DONT use that restriction for the second half.
+        if canon_colors[i] in i_colors:
+            continue
+        i_colors.add(canon_colors[i])
+
+        for j in range(0, len(unpaired_B_nodes)):
+            new_graph = nx.Graph(current_graph)
+            new_graph.add_edge(unpaired_A_nodes[i], unpaired_B_nodes[j])
+            new_A = unpaired_A_nodes[0:i] + unpaired_A_nodes[i+1:]
+            new_B = unpaired_B_nodes[0:j] + unpaired_B_nodes[j+1:]
+            fancy_enumerate_n_choose_k_graph_pairings_helper(new_graph, new_A, new_B,\
+                bank_of_already_seen_graph_types, finished_graphs)
 
 def enumerate_n_choose_k_graph_pairings_helper(N, current_graph, unpaired_A_nodes,\
         unpaired_B_nodes, bank_of_already_seen_graph_types, finished_graphs):
@@ -38,6 +89,7 @@ def enumerate_n_choose_k_graph_pairings_helper(N, current_graph, unpaired_A_node
         finished_graphs.append(current_graph)
         if len(finished_graphs) % 100 == 0:
             print(len(finished_graphs))
+            sys.stdout.flush()
         return
 
     coloring = KTupleTest(current_graph, k=N, mode="Servant").internal_labels
@@ -54,18 +106,10 @@ def enumerate_n_choose_k_graph_pairings_helper(N, current_graph, unpaired_A_node
             new_graph.add_edge(unpaired_A_nodes[i], unpaired_B_nodes[j])
             new_A = unpaired_A_nodes[0:i] + unpaired_A_nodes[i+1:]
             new_B = unpaired_B_nodes[0:j] + unpaired_B_nodes[j+1:]
-            if len(new_A) + 1 != len(unpaired_A_nodes):
-                print(new_A)
-                print(unpaired_A_nodes)
-                print(i)
-            if len(new_B) + 1 != len(unpaired_B_nodes):
-                print(new_B)
-                print(unpaired_B_nodes)
-                print(j)
             enumerate_n_choose_k_graph_pairings_helper(N, new_graph, new_A, new_B,\
                 bank_of_already_seen_graph_types, finished_graphs)
 
-graphs_yo = enumerate_n_choose_k_graph_pairings(5, 2)
+graphs_yo = enumerate_n_choose_k_graph_pairings(5, 2, N)
 print(len(graphs_yo))
 """
 for graph in graphs_yo:
@@ -78,4 +122,10 @@ for graph in graphs_yo:
 
 for i in range(0, len(graphs_yo)):
     for j in range(i, len(graphs_yo)):
-        print("%d vs %d. Predicted: %s" % (i, j, k_tuple_check(graphs_yo[i], graph_utils.permute_node_labels(graphs_yo[j]))))
+        print("%d vs %d:" % (i, j)) 
+        sys.stdout.flush()
+        for k in range(0, 10):
+            equal = k_tuple_check(graphs_yo[i], graph_utils.permute_node_labels(graphs_yo[j]))
+        print("Predicted: %s" % equal)
+        print("----")
+        sys.stdout.flush()
