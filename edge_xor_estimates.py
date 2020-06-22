2a1794c887cb84c523646a7bd05ce4c3511c7142
Steve Willowby <jhibshma@nd.edu>
2020-06-22 16:59:53 -0400

    Chebyschev and Markov Inequalities.

---
+++ edge_xor_estimates.py
@@ -0,0 +1,9 @@
+# Math:
+# Chebyschev's Inequality:
+#       Let X be an (integrable?) variable with expected value u and finite non-
+#       zero variance o^2. Then for any real number k > 0,
+#       Pr(|X - u| >= ko) <= 1/k^2
+#
+# Markov's Inequality:
+#       Let X be a non-negative random variable and a > 0, then,
+#       Pr(X >= a) <= E(X) / a
